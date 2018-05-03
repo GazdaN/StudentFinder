3b1072f42214f7e271f752548abd3a94c4025a9b
clifu <d.fronia@o2.pl>
2018-05-03 14:22:43 +0200

    Add StartActivity - WelcomePage for StudentFinder.

--- app/src/main/java/com/umikowicze/studentfinder/MainActivity.java
+++ app/src/main/java/com/umikowicze/studentfinder/MainActivity.java
@@ -1,13 +1,37 @@
 package com.umikowicze.studentfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        //It will be null if user is not signed in -> it will switch screen to WelcomeScreen (startActivity)
+        if(currentUser == null)
+        {
+            Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
+    }
+
 }
