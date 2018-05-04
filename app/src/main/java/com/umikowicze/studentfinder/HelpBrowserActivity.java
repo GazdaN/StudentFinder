8a47caf5740323f11e4b5a32e683c09dc7c00414
Krzysztof Zając <krzysztofzajac111@gmail.com>
2018-05-04 13:42:30 +0200

    Add browsing database for help in specific area in HelpBrowserActivity

--- app/src/main/java/com/umikowicze/studentfinder/HelpBrowserActivity.java
+++ app/src/main/java/com/umikowicze/studentfinder/HelpBrowserActivity.java
@@ -4,15 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -26,6 +30,7 @@ public class HelpBrowserActivity extends AppCompatActivity{
     private AutoCompleteTextView searchField;
     private ListView helpersListView;
     private HelperAdapter helperAdapter;
+    private Button button;
 
     private FirebaseDatabase mFirebaseReference;
     private DatabaseReference mHelpersDatabaseReference;
@@ -37,6 +42,9 @@ public class HelpBrowserActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_browser);
+
+        mFirebaseReference = FirebaseDatabase.getInstance();
+        mHelpersDatabaseReference = mFirebaseReference.getReference();
 
         areas = new ArrayList<>();
         areas.addAll(Arrays.asList(getResources().getStringArray(R.array.areas)));
@@ -52,20 +60,47 @@ public class HelpBrowserActivity extends AppCompatActivity{
         helpersListView = findViewById(R.id.helpersListView);
         helpersListView.setAdapter(helperAdapter);
 
-        mFirebaseReference = FirebaseDatabase.getInstance();
-        mHelpersDatabaseReference = mFirebaseReference.getReference().child("Helpers");
-
-        mHelpersDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot dss : dataSnapshot.getChildren()) {
-                    Helper helper = dss.getValue(Helper.class);
-                    helperAdapter.add(helper);
-                }
-            }
+            public void onClick(View view) {
+                helperAdapter.clear();
+                String area = searchField.getText().toString();
+                Query query = mHelpersDatabaseReference.child("HelpOffers").orderByChild("area").equalTo(area);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            // dataSnapshot is the "issue" node with all children with id 0
+                            for (DataSnapshot dss : dataSnapshot.getChildren()) {
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                                HelpOffer helpOffer = dss.getValue(HelpOffer.class);
+                                Query query2 = mHelpersDatabaseReference.child("Helpers").orderByKey().equalTo(helpOffer.getUserid());
+                                query2.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                        for (DataSnapshot dss2 : dataSnapshot.getChildren()) {
+                                            Helper helper = dss2.getValue(Helper.class);
+                                            helperAdapter.add(helper);
+                                        }
+                                    }
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        }
+                        else
+                            Toast.makeText(HelpBrowserActivity.this, "Brak użytkowników oferujących pomoc w tym zakresie.", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
 
             }
         });
