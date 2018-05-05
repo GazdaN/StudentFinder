0f217fea2b10ad63c5e44fc5137008759c34c66c
clifu <d.fronia@o2.pl>
2018-05-05 12:59:07 +0200

    Add information about Chat to the database.

--- app/src/main/java/com/umikowicze/studentfinder/ChatActivity.java
+++ app/src/main/java/com/umikowicze/studentfinder/ChatActivity.java
@@ -6,16 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,6 +38,8 @@ public class ChatActivity extends AppCompatActivity {
     private ImageButton mAddStuffButton;
     private ImageButton mSendMessageButton;
     private CircleImageView mcircleImageView;
+    private FirebaseAuth mAuth;
+    private  String mCurrentUserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +57,9 @@ public class ChatActivity extends AppCompatActivity {
 
 
         mRootReference = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUserID = mAuth.getCurrentUser().getUid();
+
         mRootReference.child("Helpers").child(mChatUserId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -71,5 +82,39 @@ public class ChatActivity extends AppCompatActivity {
         mNewMessageText = findViewById(R.id.newMessageTextView);
         mAddStuffButton = findViewById(R.id.addStuffImageButton);
         mSendMessageButton = findViewById(R.id.sendMessageButton);
+
+        mRootReference.child("Chat").child(mCurrentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.hasChild(mChatUserId))
+                {
+
+                    Map chatAddMap = new HashMap();
+                    chatAddMap.put("seen", false);
+                    chatAddMap.put("timestamp", ServerValue.TIMESTAMP);
+
+                    Map chatUserMap = new HashMap();
+                    chatUserMap.put("Chat/"+mCurrentUserID+"/"+mChatUserId,chatAddMap);
+                    chatUserMap.put("Chat/"+mChatUserId+"/"+mCurrentUserID,chatAddMap);
+
+                    mRootReference.updateChildren(chatUserMap, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+
+                            if(databaseError != null)
+                            {
+                                Log.d("CHAT_LOG",databaseError.getMessage().toString());
+                            }
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
