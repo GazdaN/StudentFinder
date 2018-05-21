87a51e06265b788c32cb72cd01757f00a84825a8
Natalia <nataliagaz.95@gmail.com>
2018-05-21 19:50:43 +0200

    register ...

--- app/src/main/java/com/umikowicze/studentfinder/RegisterUserActivity.java
+++ app/src/main/java/com/umikowicze/studentfinder/RegisterUserActivity.java
@@ -1,28 +1,54 @@
 package com.umikowicze.studentfinder;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterUserActivity extends AppCompatActivity {
 
-    private FirebaseAuth auth;
+    private static final String TAG = "mno";
+    private static final int RC_PHOTO_PICKER = 2;
 
     private EditText inputEmail;
+    private String email;
     private EditText inputPassword;
+    private String password;
+    private String photoUrl;
     private EditText inputNickName;
-    private Button registerButton;
+    private String nickName;
     private boolean mathematics_area;
     private boolean physics_area;
     private boolean coding_area;
     private boolean electronics_area;
+
+    private Button registerButton;
+    private ImageButton mPhotoPickerButton;
+
+    //Firebase instance variables
+    private FirebaseAuth auth;
+    private DatabaseReference mDataBase;
 
 
     @Override
@@ -31,19 +57,33 @@ public class RegisterUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_user);
 
         auth = FirebaseAuth.getInstance();
+
+
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.passwordd);
         inputNickName = (EditText) findViewById(R.id.nickName);
         registerButton = (Button) findViewById(R.id.register_button);
+//        mPhotoPickerButton = (ImageButton) findViewById(R.id.photo_select);
+//
+//        mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//                intent.setType("image/jpeg");
+//                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+//                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+//            }
+//        });
 
         registerButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
-                String email = inputEmail.getText().toString().trim();
-                String password = inputPassword.getText().toString().trim();
-                String nickName = inputNickName.getText().toString().trim();
+                email = inputEmail.getText().toString().trim();
+                password = inputPassword.getText().toString().trim();
+                nickName = inputNickName.getText().toString().trim();
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
@@ -59,14 +99,48 @@ public class RegisterUserActivity extends AppCompatActivity {
                     return;
                 }
 
+
+                //onCheckboxClicked(view);
+                registerUser(email, photoUrl, password, nickName);
+
                 //onCheckboxClicked();
-
-
 
 
                 //Toast.makeText(RegisterUserActivity.this, "You have been registered.", Toast.LENGTH_LONG).show();
             }
         });
+
+
+    }
+
+    private void registerUser(String mEmail, String mphotoUrl, String mPassword, String mNickName) {
+
+        // Helper helper = new Helper(mNickName, mphotoUrl, mStars, mRatings, mLocation );
+        //mDataBase= FirebaseDatabase.getInstance().getReference();
+        //mDataBase.child("Helpers").child("name").setValue(mNickName);
+
+        mphotoUrl = "fddsfsdf";
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser user = auth.getCurrentUser();
+                            Intent mainIntent = new Intent(RegisterUserActivity.this, MainActivity.class);
+                            //mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(mainIntent);
+                            finish();
+                        } else {
+                            // If registering in fails
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterUserActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            // updateUI(null);
+                        }
+
+                    }
+                });
 
 
     }
@@ -104,4 +178,35 @@ public class RegisterUserActivity extends AppCompatActivity {
 
         }
     }
-}
+
+
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+//            final Uri selectedIamgeUri = data.getData();
+//
+//            //Refrence to store photo file
+//            StorageReference photoRef = mProfilePhotoRefrence.child(selectedIamgeUri.getLastPathSegment());
+//
+//            //Uploading photo file to Firebase storage
+//
+//
+//            photoRef.putFile(selectedIamgeUri).addOnSuccessListener
+//                    (this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                            Uri downloadUrl = taskSnapshot.getDownloadUrl();
+//                            //TODO: Wyswietlanie zdjecia profilowego
+//                            //mPhotoPickerButton.setImageResource();
+//                        }
+//                    });
+//
+//
+//        }
+
+
+    }
+
+
+
